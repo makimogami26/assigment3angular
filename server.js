262aea4f875cfbfe96afb62378adfe7e68dd39b6
makimogami26 <dspryd26@gmail.com>
2021-05-16 21:45:40 +0700

    assigment3angular

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
 }
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(requireHTTPS);
-app.use(express.static('./dist/assignment3'));
+app.use(express.static("./dist/shopping-cart"));
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/assignment3/'}
-  );
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/shopping-cart/" });
 });
 app.listen(process.env.PORT || 8080);
